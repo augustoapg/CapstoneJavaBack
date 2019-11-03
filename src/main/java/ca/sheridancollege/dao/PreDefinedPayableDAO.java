f2d723e8586301334bbbd0be33fc8e344accad12
Augusto Peres <augustoapg@gmail.com>
2019-11-03 16:11:29 -0500

    Fixed bug with edit PreDefPayable

--- src/main/java/ca/sheridancollege/dao/PreDefinedPayableDAO.java
+++ src/main/java/ca/sheridancollege/dao/PreDefinedPayableDAO.java
@@ -91,7 +91,7 @@ public class PreDefinedPayableDAO {
 		try {
 			PreDefinedPayable existingPayable = getPredefinedPayableByCategory(preDefinedPayable.getCategory());
 			
-			if (existingPayable != null) {
+			if (existingPayable == null) {
 				session.update(preDefinedPayable);
 			} else {
 				throw new Exception("Pre Defined Payable already exists if category " + preDefinedPayable.getCategory());
